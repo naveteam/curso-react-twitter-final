ee2dbf8e5f93a040025d780f956829ede8f290f3
João Pedro Bretanha <joaopedro@nave.rs>
2017-11-23 21:10:44 -0200

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+## Estrutura inicial para o curso de react
+
+Instale as dependências:
+
+* `yarn install` ou `npm install`
+
+Rode o projeto:
+
+* `yarn start` ou `npm start`
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{textAlign: 'center'}}>Bem-vindo ao curso de react</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
