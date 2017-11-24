6460c3e531e767417204be838c46ce7bdd0dc0ff
jpbretanha <jpbretanha@Matheuss-Mac-mini.local>
2017-11-24 12:03:35 -0200

    updated service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import api from './api';
 
-export const login = data => api.post('/login', data);
+export const loginUser = data => api.post('/login', data);
 
 export const signUp = user => api.post('/user/create', user);
 
